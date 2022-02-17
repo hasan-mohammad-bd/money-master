d95c5134f327b4452317ed730495acccfe566730
hasan-mohammad-bd <hasan.md.east@gmail.com>
2022-02-17 11:15:22 +0600

    first section done

---
+++ script.js
@@ -0,0 +1,62 @@
+//get input value
+function getInput(inputNumber){
+    let theInput = document.getElementById(inputNumber);
+    let inputText = theInput.value;
+    let inputValue = parseFloat(inputText);
+    theInput.value = "";
+    return inputValue;       
+}
+
+function textField(textField, isAdd, inputValue, amount){
+    let theField = document.getElementById(textField);
+    let theFieldText = theField.innerText;
+    let previousValue = parseFloat(theFieldText);
+    if(isAdd == true){
+        let totalValue = previousValue + amount;
+        theField.innerText = totalValue;
+        return theField.innerText
+    }
+    else if (isAdd == false){
+        let totalValue = inputValue - amount;
+        theField.innerText = totalValue;
+        return theField.innerText;
+    }
+}
+
+function calPercentage(inputValue, percentage){
+    let thePercentage = inputValue / percentage;
+    return thePercentage;
+
+}
+
+
+
+
+
+
+
+
+
+
+//calculate button function
+
+document.getElementById("cal-btn").addEventListener("click", function(){
+    let incomeInput = getInput("income-input");
+    document.getElementById("total-income").innerText = incomeInput;
+    let foodInput = getInput("food-expense-input");
+    let rentInput = getInput("rent-expense-input");
+    let clothesInput = getInput("clothes-expense-input");
+    let totalExpense = foodInput + rentInput + clothesInput;
+    document.getElementById("total-expense").innerText = totalExpense;
+    let remainingBalance = textField("total-balance",false,incomeInput,totalExpense)
+})
+
+//save button function
+document.getElementById("save-btn").addEventListener("click", function(){
+    let totalIncome = document.getElementById("total-income").innerText;
+    let totalIncomeValue = parseFloat(totalIncome);
+    let percentageInput = getInput("save-input");
+    let calculatePercentage = calPercentage(totalIncomeValue, percentageInput);
+    console.log(calculatePercentage);
+    document.getElementById("saiving-amount").innerText = calculatePercentage;
+})
