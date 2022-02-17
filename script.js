1f784345b9d6086539a0fcb5bb14b0b67c6d1a89
hasan-mohammad-bd <hasan.md.east@gmail.com>
2022-02-17 17:47:51 +0600

    adding comment

--- script.js
+++ script.js
@@ -1,48 +1,65 @@
-//get input value
+//input value
 function getInput(inputNumber){
     let theInput = document.getElementById(inputNumber);
     let inputText = theInput.value;
     let inputValue = parseFloat(inputText);
     theInput.value = "";
-    return inputValue;       
+    return inputValue;    
 }
 
-function textField(textField, isAdd, inputValue, amount){
+//innerText
+function textField(textField, inputValue, amount){
     let theField = document.getElementById(textField);
-    let theFieldText = theField.innerText;
-    let previousValue = parseFloat(theFieldText);
-    if(isAdd == true){
-        let totalValue = previousValue + amount;
-        theField.innerText = totalValue;
-        return theField.innerText
-    }
-    else if (isAdd == false){
-        let totalValue = inputValue - amount;
-        theField.innerText = totalValue;
-        return theField.innerText;
-    }
+    let totalValue = inputValue - amount;
+    theField.innerText = totalValue;
+    return theField.innerText;
+    
 }
-
+//percentage of income
 function calPercentage(inputValue, percentage){
     let thePercentage = (inputValue / 100)* percentage;
     return thePercentage;
 
 }
 
-
-
-
+//error message
+document.getElementById("alert-msg1").style.display = "none";
+document.getElementById("alert-msg2").style.display = "none";
+document.getElementById("alert-msg3").style.display = "none";
+document.getElementById("alert-msg4").style.display = "none";
+document.getElementById("alert-msg5").style.display = "none";
 //calculate button function
-
 document.getElementById("cal-btn").addEventListener("click", function(){
     let incomeInput = getInput("income-input");
-    document.getElementById("total-income").innerText = incomeInput;
-    let foodInput = getInput("food-expense-input");
-    let rentInput = getInput("rent-expense-input");
-    let clothesInput = getInput("clothes-expense-input");
-    let totalExpense = foodInput + rentInput + clothesInput;
-    document.getElementById("total-expense").innerText = totalExpense;
-    let remainingBalance = textField("total-balance",false,incomeInput,totalExpense)
+    //error catching
+    if(incomeInput > 0){
+        document.getElementById("alert-msg1").style.display = "none"
+        document.getElementById("total-income").innerText = incomeInput;
+        let foodInput = getInput("food-expense-input");       
+        let rentInput = getInput("rent-expense-input");
+        let clothesInput = getInput("clothes-expense-input");
+        //error catching
+        if(foodInput > 0 && rentInput > 0 && clothesInput > 0 ){
+            document.getElementById("alert-msg2").style.display = "none";
+            let totalExpense = foodInput + rentInput + clothesInput;
+            //error catching
+            if(totalExpense <= incomeInput){
+                document.getElementById("alert-msg3").style.display = "none";
+                document.getElementById("total-expense").innerText = totalExpense;
+                let remainingBalance = textField("total-balance",incomeInput,totalExpense);
+            } else {
+                document.getElementById("alert-msg3").style.display = "block";
+            }
+
+        } else {
+            document.getElementById("alert-msg2").style.display = "block";
+        }
+
+    }
+    else{
+        document.getElementById("alert-msg1").style.display = "block";
+    }
+
 })
 
 //save button function
@@ -50,9 +67,23 @@ document.getElementById("save-btn").addEventListener("click", function(){
     let totalIncome = document.getElementById("total-income").innerText;
     let totalIncomeValue = parseFloat(totalIncome);
     let percentageInput = getInput("save-input");
-    let calculatePercentage = calPercentage(totalIncomeValue, percentageInput);
-    document.getElementById("saving-amount").innerText = calculatePercentage;
-    let totalBalance = document.getElementById("total-balance").innerText;
-    let totalBalanceAfterSaving = totalBalance - calculatePercentage;
-    document.getElementById("remaining-balance").innerText = totalBalanceAfterSaving;
+    //error catching
+    if(percentageInput > 0){
+        document.getElementById("alert-msg4").style.display = "none";
+        let calculatePercentage = calPercentage(totalIncomeValue, percentageInput);
+        document.getElementById("saving-amount").innerText = calculatePercentage;
+        let totalBalance = document.getElementById("total-balance").innerText;
+        //error catching
+        if(calculatePercentage <= totalBalance) {
+            document.getElementById("alert-msg5").style.display = "none";
+            let totalBalanceAfterSaving = totalBalance - calculatePercentage;
+            document.getElementById("remaining-balance").innerText = totalBalanceAfterSaving;
+        } else {
+            document.getElementById("alert-msg5").style.display = "block";
+        }
+
+    } else {
+        document.getElementById("alert-msg4").style.display = "block";
+    }
+
 })
